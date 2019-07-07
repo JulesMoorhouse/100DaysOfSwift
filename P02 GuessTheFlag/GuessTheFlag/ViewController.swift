87ca9cd4b307e561c989b71d0b03bbe9626a8e29
Julian Moorhouse <julesthecoder@gmail.com>
2019-07-07 10:32:39 +0100

    Basic files and ui setup for project 2 GuessTheFlag

---
+++ P02 GuessTheFlag/GuessTheFlag/ViewController.swift
@@ -0,0 +1,23 @@
+//
+//  ViewController.swift
+//  GuessTheFlag
+//
+//  Created by Julian Moorhouse on 07/07/2019.
+//  Copyright © 2019 Mindwarp Consultancy Ltd. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    @IBOutlet var button1: UIButton!
+    @IBOutlet var button2: UIButton!
+    @IBOutlet var button3: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
