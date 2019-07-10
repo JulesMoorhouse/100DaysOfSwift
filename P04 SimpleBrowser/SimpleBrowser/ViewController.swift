8fe3f221658d8d53c1c6080698d900d647c892f9
Julian Moorhouse <julesthecoder@gmail.com>
2019-07-10 19:46:44 +0100

    Added SimpleBrowser aka Project 4 basic project

---
+++ P04 SimpleBrowser/SimpleBrowser/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SimpleBrowser
+//
+//  Created by Julian Moorhouse on 10/07/2019.
+//  Copyright © 2019 Mindwarp Consultancy Ltd. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
