516c42be523e70da76fbe32591ba374faee57f0b
Julian Moorhouse <julesthecoder@gmail.com>
2019-07-29 19:29:30 +0100

    Started new project WordScrable aka Project 5

---
+++ P05 WordScrable/WordScrable/ViewController.swift
@@ -0,0 +1,47 @@
+//
+//  ViewController.swift
+//  WordScrable
+//
+//  Created by Julian Moorhouse on 29/07/2019.
+//  Copyright © 2019 Mindwarp Consultancy Ltd. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UITableViewController {
+    var allWords = [String]()
+    var usedWords = [String]()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        if let startWordsUrl = Bundle.main.url(forResource: "start", withExtension: "txt") {
+            if let startWords = try? String(contentsOf:  startWordsUrl) {
+                allWords = startWords.components(separatedBy: "\n")
+            }
+        }
+        
+        if allWords.isEmpty {
+            allWords = ["silkworm"]
+        }
+        
+        startGame()
+    }
+
+    func startGame() {
+        title = allWords.randomElement()
+        usedWords.removeAll(keepingCapacity: true)
+        tableView.reloadData()
+    }
+    
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        return usedWords.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Word", for: indexPath)
+        cell.textLabel?.text = usedWords[indexPath.row]
+        return cell
+    }
+}
+
