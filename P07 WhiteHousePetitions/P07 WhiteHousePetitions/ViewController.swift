ba37af6c41b8e8b21db964d9750cb778a7f8da3d
Julian Moorhouse <julesthecoder@gmail.com>
2019-08-04 12:08:20 +0100

    Added second tab bar, linked up alternative json loading and added error handling

--- P07 WhiteHousePetitions/P07 WhiteHousePetitions/ViewController.swift
+++ P07 WhiteHousePetitions/P07 WhiteHousePetitions/ViewController.swift
@@ -15,13 +15,24 @@ class ViewController: UITableViewController {
         super.viewDidLoad()
         
         //Beware - this code will lock up the app while data is downloaded, but it's ok for example purposes here
-        let urlString = "https://www.hackingwithswift.com/samples/petitions-1.json"
-        
+        let urlString: String
+            
+        if navigationController?.tabBarItem.tag == 0 {
+            // urlString = "https://api.whitehouse.gov/v1/petitions.json?limit=100"
+            urlString = "https://www.hackingwithswift.com/samples/petitions-1.json"
+        } else {
+            // urlString = "https://api.whitehouse.gov/v1/petitions.json?signatureCountFloor=10000&limit=100"
+            urlString = "https://www.hackingwithswift.com/samples/petitions-2.json"
+        }
+
         if let url = URL(string: urlString) {
             if let data = try? Data(contentsOf: url) {
                 parse(json: data)
+                return
             }
         }
+        
+        showError()
     }
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
@@ -50,6 +61,12 @@ class ViewController: UITableViewController {
             petitions = jsonPetitions.results
             tableView.reloadData()
         }
+    }
+    
+    func showError() {
+        let ac = UIAlertController(title: "Loading error", message: "There was a problem loading the feed, please check your connection and try again.", preferredStyle: .alert)
+        ac.addAction(UIAlertAction(title: "OK", style: .default))
+        present(ac, animated: true)
     }
 }
 
