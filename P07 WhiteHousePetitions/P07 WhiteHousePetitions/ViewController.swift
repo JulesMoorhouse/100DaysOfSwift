eadc66a7fdd3e344fb674618bab7c68060f9645c
Julian Moorhouse <julesthecoder@gmail.com>
2019-08-04 11:19:41 +0100

    Added loading of json from hackingwithswift server and displays data in tableview.

--- P07 WhiteHousePetitions/P07 WhiteHousePetitions/ViewController.swift
+++ P07 WhiteHousePetitions/P07 WhiteHousePetitions/ViewController.swift
@@ -9,12 +9,41 @@
 import UIKit
 
 class ViewController: UITableViewController {
-
+    var petitions = [Petition]()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        //Beware - this code will lock up the app while data is downloaded, but it's ok for example purposes here
+        let urlString = "https://www.hackingwithswift.com/samples/petitions-1.json"
+        
+        if let url = URL(string: urlString) {
+            if let data = try? Data(contentsOf: url) {
+                parse(json: data)
+            }
+        }
     }
 
-
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return petitions.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
+        
+        let petition = petitions[indexPath.row]
+        cell.textLabel?.text = petition.title
+        cell.detailTextLabel?.text = petition.body
+        return cell
+    }
+    
+    func parse(json: Data) {
+        let decoder = JSONDecoder()
+        
+        if let jsonPetitions = try? decoder.decode(Petitions.self, from: json) {
+            petitions = jsonPetitions.results
+            tableView.reloadData()
+        }
+    }
 }
 
