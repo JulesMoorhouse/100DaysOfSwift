79ada2e6d36d8267a9c0e1df629fabf2eeb82abf
Julian Moorhouse <julesthecoder@gmail.com>
2019-08-09 11:24:47 +0100

    Showing / hiding penguins

--- P14 WhackAPenguin/WhackAPenguin/WhackSlot.swift
+++ P14 WhackAPenguin/WhackAPenguin/WhackSlot.swift
@@ -12,6 +12,9 @@ import SpriteKit
 class WhackSlot: SKNode {
     var charNode: SKSpriteNode!
     
+    var isVisible = false
+    var isHit = false
+    
     func configure(at position: CGPoint) {
         self.position = position
         
@@ -29,5 +32,33 @@ class WhackSlot: SKNode {
         cropNode.addChild(charNode)
         
         addChild(cropNode)
+    }
+    
+    func show(hideTime: Double) {
+        if isVisible { return }
+        
+        charNode.run(SKAction.moveBy(x: 0, y: 80, duration: 0.05))
+        isVisible = true
+        isHit = true
+        
+        if Int.random(in: 0...2) == 0 {
+            charNode.texture = SKTexture(imageNamed: "penguinGood")
+            charNode.name = "charFriend"
+        } else {
+            charNode.texture = SKTexture(imageNamed: "penguinEvil")
+            charNode.name = "charEnemy"
+        }
+        
+        DispatchQueue.main.asyncAfter(deadline: .now() + (hideTime * 3.5)) {
+            [weak self] in
+            self?.hide()
+        }
+    }
+    
+    func hide() {
+        if !isVisible { return }
+        
+        charNode.run(SKAction.moveBy(x: 0, y: -80, duration: 0.05))
+        isVisible = false
     }
 }
