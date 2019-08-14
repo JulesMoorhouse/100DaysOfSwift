ea7157e61a87cff481efd5410114328b46ad46c1
Julian Moorhouse <julesthecoder@gmail.com>
2019-08-14 12:08:35 +0100

    Added Action.js

---
+++ P19 SafariJavascriptInjection/Extension/Action.js
@@ -0,0 +1,15 @@
+var Action = function() { }
+
+Action.prototype = {
+    
+run: function(paramaters) {
+
+},
+    
+finalize: function(paramaters) {
+    
+}
+    
+};
+
+var ExtensionPreprocessingJS = new Action
