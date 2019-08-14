b2b1933b94887fb96a016000d0204dcb31f21ee8
Julian Moorhouse <julesthecoder@gmail.com>
2019-08-14 12:18:52 +0100

    Safari now passes data to extension

--- P19 SafariJavascriptInjection/Extension/Action.js
+++ P19 SafariJavascriptInjection/Extension/Action.js
@@ -3,7 +3,7 @@ var Action = function() { }
 Action.prototype = {
     
 run: function(paramaters) {
-
+    paramaters.completionFunction({"URL": document.URL, "title": document.title});
 },
     
 finalize: function(paramaters) {
