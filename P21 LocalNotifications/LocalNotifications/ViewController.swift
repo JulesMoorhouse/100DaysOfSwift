5c0b07baacba8a99b3cbe9b09cae174771ff1a65
Julian Moorhouse <julesthecoder@gmail.com>
2019-08-15 15:37:21 +0100

    Completed project 21 challenge changes

--- P21 LocalNotifications/LocalNotifications/ViewController.swift
+++ P21 LocalNotifications/LocalNotifications/ViewController.swift
@@ -15,7 +15,7 @@ class ViewController: UIViewController, UNUserNotificationCenterDelegate {
         super.viewDidLoad()
         
         navigationItem.leftBarButtonItem = UIBarButtonItem(title: "Register", style: .plain, target: self, action: #selector(registerLocal))
-        navigationItem.rightBarButtonItem = UIBarButtonItem(title: "Schedule", style: .plain, target: self, action: #selector(scheduleLocal))
+        navigationItem.rightBarButtonItem = UIBarButtonItem(title: "Schedule", style: .plain, target: self, action: #selector(scheduleLocalAsk))
     }
 
     @objc func registerLocal() {
@@ -31,17 +31,31 @@ class ViewController: UIViewController, UNUserNotificationCenterDelegate {
             }
         }
     }
+    
+    @objc func scheduleLocalAsk() {
+        let ac = UIAlertController(title: "Action type", message: nil, preferredStyle: .actionSheet)
+        ac.addAction(UIAlertAction(title: "alarm", style: .default, handler: {
+            
+            [weak self] (alert) in
+            self?.scheduleLocal(category: "alarm", title: "Late wake up call", message: "The early bird catches the worm, but the second mouse gets the cheese.")
+        }))
+        ac.addAction(UIAlertAction(title: "test", style: .default, handler: {
+            [weak self] (alert) in
+            self?.scheduleLocal(category: "test", title: "Test Notificiation", message: "Testing")
+        }))
+        present(ac, animated: true)
+    }
 
-    @objc func scheduleLocal() {
+    func scheduleLocal(category: String, title: String, message: String) {
         registerCategories()
         
         let center = UNUserNotificationCenter.current()
         center.removeAllPendingNotificationRequests() // don't have to use this
         
         let content = UNMutableNotificationContent()
-        content.title = "Late wake up call"
-        content.body = "The early bird catches the worm, but the second mouse gets the cheese."
-        content.categoryIdentifier = "alarm" // identify for use on receipt
+        content.title = title
+        content.body = message
+        content.categoryIdentifier = category // identify for use on receipt
         content.userInfo = ["customData": "fizzbuzz"] // identify for use on receipt
         content.sound = .default
         
@@ -51,7 +65,9 @@ class ViewController: UIViewController, UNUserNotificationCenterDelegate {
         
 //        let trigger = UNCalendarNotificationTrigger(dateMatching: dateComponents, repeats: true)
         
-        let trigger = UNTimeIntervalNotificationTrigger(timeInterval: 5, repeats: false)
+        let interval = 5
+        
+        let trigger = UNTimeIntervalNotificationTrigger(timeInterval: TimeInterval(interval), repeats: false)
         
         // remember you can use the same identifier which will update a notification rather than send new ones, e.g. goals in a football match
         let request = UNNotificationRequest(identifier: UUID().uuidString, content: content, trigger: trigger)
@@ -66,9 +82,14 @@ class ViewController: UIViewController, UNUserNotificationCenterDelegate {
         center.delegate = self
         
         let show = UNNotificationAction(identifier: "show", title: "Tell me more", options: .foreground)
-        let category = UNNotificationCategory(identifier: "alarm", actions: [show], intentIdentifiers: [], options: [])
+        let showTest = UNNotificationAction(identifier: "test", title: "Test", options: .foreground)
         
-        center.setNotificationCategories([category])
+        let remindMe = UNNotificationAction(identifier: "remindme", title: "Remind me later", options: .foreground)
+        
+        let category = UNNotificationCategory(identifier: "alarm", actions: [show, remindMe], intentIdentifiers: [], options: [])
+        let testCategory = UNNotificationCategory(identifier: "test", actions: [showTest], intentIdentifiers: [], options: [])
+
+        center.setNotificationCategories([category, testCategory])
     }
     
     func userNotificationCenter(_ center: UNUserNotificationCenter, didReceive response: UNNotificationResponse, withCompletionHandler completionHandler: @escaping () -> Void) {
@@ -84,8 +105,19 @@ class ViewController: UIViewController, UNUserNotificationCenterDelegate {
                 
             case "show":
                 // The user swiped the notification, tapped "View", then "Tell me more" appeared
-                print("Show more information...")
+                let ac = UIAlertController(title: "Message", message: "Show more information...", preferredStyle: .alert)
+                ac.addAction(UIAlertAction(title: "OK", style: .cancel))
+                present(ac, animated: true)
+            case "test":
+                let ac = UIAlertController(title: "Message", message: "Test custom", preferredStyle: .alert)
+                ac.addAction(UIAlertAction(title: "OK", style: .cancel))
+                present(ac, animated: true)
+            case "remindme":
+                let ac = UIAlertController(title: "Message", message: "Remind me later, will now remind in 24 hours", preferredStyle: .alert)
+                ac.addAction(UIAlertAction(title: "OK", style: .cancel))
+                present(ac, animated: true)
                 
+                scheduleLocal(category: "remind24", title: "Reminder", message: "This is your 24 hour reminder")
             default:
                 break
                 
