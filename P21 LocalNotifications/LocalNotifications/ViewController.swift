0a515d7d584ffe3091d6547622e125bfa66cc4bd
Julian Moorhouse <julesthecoder@gmail.com>
2019-08-15 12:50:17 +0100

    Added basic notification

--- P21 LocalNotifications/LocalNotifications/ViewController.swift
+++ P21 LocalNotifications/LocalNotifications/ViewController.swift
@@ -7,14 +7,57 @@
 //
 
 import UIKit
+import UserNotifications
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        navigationItem.leftBarButtonItem = UIBarButtonItem(title: "Register", style: .plain, target: self, action: #selector(registerLocal))
+        navigationItem.rightBarButtonItem = UIBarButtonItem(title: "Schedule", style: .plain, target: self, action: #selector(scheduleLocal))
     }
 
+    @objc func registerLocal() {
+        let center = UNUserNotificationCenter.current()
+        
+        center.requestAuthorization(options: [.alert, .badge, .sound]) {
+            granted, error in
+            if granted {
+                print("Yay!")
+            } else {
+                // Remember you'll need to reset the simulator content
+                print("D'oh!")
+            }
+        }
+    }
 
+    @objc func scheduleLocal() {
+        let center = UNUserNotificationCenter.current()
+        center.removeAllPendingNotificationRequests() // don't have to use this
+        
+        let content = UNMutableNotificationContent()
+        content.title = "Late wake up call"
+        content.body = "The early bird catches the worm, but the second mouse gets the cheese."
+        content.categoryIdentifier = "alarm" // identify for use on receipt
+        content.userInfo = ["customData": "fizzbuzz"] // identify for use on receipt
+        content.sound = .default
+        
+        var dateComponents = DateComponents()
+        dateComponents.hour = 10
+        dateComponents.minute = 30
+        
+//        let trigger = UNCalendarNotificationTrigger(dateMatching: dateComponents, repeats: true)
+        
+        let trigger = UNTimeIntervalNotificationTrigger(timeInterval: 5, repeats: false)
+        
+        // remember you can use the same identifier which will update a notification rather than send new ones, e.g. goals in a football match
+        let request = UNNotificationRequest(identifier: UUID().uuidString, content: content, trigger: trigger)
+        
+        center.add(request)
+        
+        // remember to immediate lock the device or command + L on the simulator, so the notification is shown after those 4 seconds
+    }
 }
+
 
