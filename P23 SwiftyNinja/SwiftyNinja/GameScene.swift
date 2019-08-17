e501fcd00370b7e4bedab724fa7c386fe64f3dd9
Julian Moorhouse <julesthecoder@gmail.com>
2019-08-17 09:01:23 +0100

    Added stub project 23 aka SwiftyNinja

---
+++ P23 SwiftyNinja/SwiftyNinja/GameScene.swift
@@ -0,0 +1,20 @@
+//
+//  GameScene.swift
+//  SwiftyNinja
+//
+//  Created by Julian Moorhouse on 17/08/2019.
+//  Copyright © 2019 Mindwarp Consultancy Ltd. All rights reserved.
+//
+
+import SpriteKit
+
+class GameScene: SKScene {
+    
+    private var label : SKLabelNode?
+    private var spinnyNode : SKShapeNode?
+    
+    override func didMove(to view: SKView) {
+       
+    }
+
+}
