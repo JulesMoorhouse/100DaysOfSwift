dd0d6fa3e04bba19dda4170d8d574555a4dd4f26
Julian Moorhouse <julesthecoder@gmail.com>
2019-08-29 20:18:08 +0100

    Saving to keychain

--- P28 SecretSwift/SecretSwift/ViewController.swift
+++ P28 SecretSwift/SecretSwift/ViewController.swift
@@ -10,17 +10,22 @@ import UIKit
 
 class ViewController: UIViewController {
     @IBOutlet var secret: UITextView!
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
+        title = "Nothing to see here"
+        
         let notificationCenter = NotificationCenter.default
         notificationCenter.addObserver(self, selector: #selector(adjustForKeyboard), name: UIResponder.keyboardWillHideNotification, object: nil)
         notificationCenter.addObserver(self, selector: #selector(adjustForKeyboard), name: UIResponder.keyboardWillChangeFrameNotification, object: nil)
-    }
-
-    @IBAction func authenticateTapped(_ sender: Any) {
         
+        // when leave app
+        notificationCenter.addObserver(self, selector: #selector(saveSecretMessage), name: UIApplication.willResignActiveNotification, object: nil)
+    }
+    
+    @IBAction func authenticateTapped(_ sender: Any) {
+        unlockSecretMessage()
     }
     
     @objc func adjustForKeyboard(notification: Notification) {
@@ -41,4 +46,21 @@ class ViewController: UIViewController {
         secret.scrollRangeToVisible(selectedRange)
     }
     
+    func unlockSecretMessage() {
+        secret.isHidden = false
+        title = "Secret stuff!"
+        
+        if let text = KeychainWrapper.standard.string(forKey: "SecretMessage") {
+            secret.text = text
+        }
+    }
+    
+    @objc func saveSecretMessage() {
+        guard secret.isHidden == false else { return }
+        
+        KeychainWrapper.standard.set(secret.text, forKey: "SecretMessage")
+        secret.resignFirstResponder()
+        secret.isHidden = true
+        title = "Nothing to see here"
+    }
 }
