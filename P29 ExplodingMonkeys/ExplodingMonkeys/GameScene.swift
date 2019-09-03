8ca5aa6fce02be6eb2e675f24bc5c9e930ba2d83
Julian Moorhouse <julesthecoder@gmail.com>
2019-09-03 21:21:12 +0100

    Added players and banana + assets

--- P29 ExplodingMonkeys/ExplodingMonkeys/GameScene.swift
+++ P29 ExplodingMonkeys/ExplodingMonkeys/GameScene.swift
@@ -6,8 +6,8 @@
 //  Copyright © 2019 Mindwarp Consultancy Ltd. All rights reserved.
 //
 
-import SpriteKit
 import GameplayKit
+import SpriteKit
 
 enum CollisionTypes: UInt32 {
     case banana = 1
@@ -19,9 +19,16 @@ class GameScene: SKScene {
     var buildings = [BuildingNode]()
     weak var viewController: GameViewController?
     
+    var player1: SKSpriteNode!
+    var player2: SKSpriteNode!
+    var banana: SKSpriteNode!
+    
+    var currentPlayer = 1
+    
     override func didMove(to view: SKView) {
         backgroundColor = UIColor(hue: 0.669, saturation: 0.99, brightness: 0.67, alpha: 1)
         createBuildings()
+        createPlayers()
     }
     
     func createBuildings() {
@@ -41,6 +48,93 @@ class GameScene: SKScene {
     }
     
     func launch(angle: Int, velocity: Int) {
+        let speed = Double(velocity) / 10
+        let radians = deg2rad(degrees: angle)
         
+        if banana != nil {
+            banana.removeFromParent()
+            banana = nil
+        }
+        
+        banana = SKSpriteNode(imageNamed: "banana")
+        banana.name = "banana"
+        banana.physicsBody = SKPhysicsBody(circleOfRadius: banana.size.width / 2)
+        banana.physicsBody?.categoryBitMask = CollisionTypes.banana.rawValue
+        // can hit buildings or players
+        banana.physicsBody?.collisionBitMask = CollisionTypes.building.rawValue | CollisionTypes.player.rawValue
+        // can bounce off them and tell us when it does so
+        banana.physicsBody?.contactTestBitMask = CollisionTypes.building.rawValue | CollisionTypes.player.rawValue
+        // very slow / use selectively - small object
+        banana.physicsBody?.usesPreciseCollisionDetection = true
+        addChild(banana)
+        
+        if currentPlayer == 1 {
+            // arm position of throw monkey
+            banana.position = CGPoint(x: player1.position.x - 30, y: player1.position.y + 40)
+            banana.physicsBody?.angularVelocity = -20
+            
+            let raiseArm = SKAction.setTexture(SKTexture(imageNamed: "player1Throw"))
+            let lowerArm = SKAction.setTexture(SKTexture(imageNamed: "player"))
+            let pause = SKAction.wait(forDuration: 0.15)
+            let sequence = SKAction.sequence([raiseArm, pause, lowerArm])
+            player1.run(sequence)
+            
+            // give banana a push to the right
+            let impulse = CGVector(dx: cos(radians) * speed, dy: sin(radians) * speed)
+            banana.physicsBody?.applyImpulse(impulse)
+        } else {
+            banana.position = CGPoint(x: player2.position.x + 30, y: player2.position.y + 40)
+            banana.physicsBody?.angularVelocity = 20
+            
+            let raiseArm = SKAction.setTexture(SKTexture(imageNamed: "player2Throw"))
+            let lowerArm = SKAction.setTexture(SKTexture(imageNamed: "player"))
+            let pause = SKAction.wait(forDuration: 0.15)
+            let sequence = SKAction.sequence([raiseArm, pause, lowerArm])
+            player2.run(sequence)
+            
+            // give banana a push to the left
+            let impulse = CGVector(dx: cos(radians) * -speed, dy: sin(radians) * speed)
+            banana.physicsBody?.applyImpulse(impulse)
+        }
+    }
+    
+    func createPlayers() {
+        player1 = SKSpriteNode(imageNamed: "player")
+        player1.name = "player1"
+        player1.physicsBody = SKPhysicsBody(circleOfRadius: player1.size.width / 2)
+        
+        // they are a player
+        player1.physicsBody?.categoryBitMask = CollisionTypes.player.rawValue
+        // bounce off bananas
+        player1.physicsBody?.collisionBitMask = CollisionTypes.banana.rawValue
+        // tell us when you hit bananas
+        player1.physicsBody?.contactTestBitMask = CollisionTypes.banana.rawValue
+        // don't let them be moved around by gravity
+        player1.physicsBody?.isDynamic = false
+        
+        // not the building that's slightly off the screen
+        let player1Building = buildings[1]
+        player1.position = CGPoint(x: player1Building.position.x, y: player1Building.position.y + ((player1Building.size.height + player1.size.height) / 2))
+        
+        addChild(player1)
+        
+        player2 = SKSpriteNode(imageNamed: "player")
+        player2.name = "player2"
+        player2.physicsBody = SKPhysicsBody(circleOfRadius: player2.size.width / 2)
+        
+        player2.physicsBody?.categoryBitMask = CollisionTypes.player.rawValue
+        player2.physicsBody?.collisionBitMask = CollisionTypes.banana.rawValue
+        player2.physicsBody?.contactTestBitMask = CollisionTypes.banana.rawValue
+        player2.physicsBody?.isDynamic = false
+        
+        let player2Building = buildings[buildings.count - 2]
+        player2.position = CGPoint(x: player2Building.position.x, y: player2Building.position.y + ((player2Building.size.height + player2.size.height) / 2))
+        
+        addChild(player2)
+    }
+    
+    func deg2rad(degrees: Int) -> Double {
+        // degrees to radians
+        return Double(degrees) * .pi / 180
     }
 }
