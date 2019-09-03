bda423fb2f625224dd84a99d3965be3db6402dbb
Julian Moorhouse <julesthecoder@gmail.com>
2019-09-03 19:49:02 +0100

    Added buildings

--- P29 ExplodingMonkeys/ExplodingMonkeys/GameScene.swift
+++ P29 ExplodingMonkeys/ExplodingMonkeys/GameScene.swift
@@ -9,9 +9,33 @@
 import SpriteKit
 import GameplayKit
 
-class GameScene: SKScene {
+enum CollisionTypes: UInt32 {
+    case banana = 1
+    case building = 2
+    case player = 4
+}
 
-    override func didMove(to view: SKView) {
+class GameScene: SKScene {
+    var buildings = [BuildingNode]()
     
+    override func didMove(to view: SKView) {
+        backgroundColor = UIColor(hue: 0.669, saturation: 0.99, brightness: 0.67, alpha: 1)
+        createBuildings()
+    }
+    
+    func createBuildings() {
+        var currentX: CGFloat = -15
+        
+        while currentX < 1024 {
+            let size = CGSize(width: Int.random(in: 2...4) * 40, height: Int.random(in: 300...600))
+            currentX += size.width + 2
+            
+            let building = BuildingNode(color: .red, size: size)
+            // from centre
+            building.position = CGPoint(x: currentX - (size.width / 2), y: size.height / 2)
+            building.setup()
+            addChild(building)
+            buildings.append(building)
+        }
     }
 }
